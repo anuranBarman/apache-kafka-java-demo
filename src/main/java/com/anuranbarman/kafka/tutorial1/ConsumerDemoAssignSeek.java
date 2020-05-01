f5c6d2cfd4a10b52301ac7d10ca3086cea0b0354
anuranBarman <anuranbarman2k15@rediffmail.com>
2020-05-01 22:38:12 +0530

    Added Assign and Seek

--- src/main/java/com/anuranbarman/kafka/tutorial1/ConsumerDemoAssignSeek.java
+++ src/main/java/com/anuranbarman/kafka/tutorial1/ConsumerDemoAssignSeek.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,27 +16,40 @@ import java.util.Properties;
 public class ConsumerDemoAssignSeek {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(ConsumerDemoAssignSeek.class.getName());
-        String groupId = "my-forth-application";
 
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
 
         KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
 
-        consumer.subscribe(Collections.singleton("first_topic"));
+        TopicPartition topicPartition = new TopicPartition("first_topic",0);
+        long offset = 15L;
 
-        while (true) {
+        consumer.assign(Collections.singleton(topicPartition));
+
+        consumer.seek(topicPartition,offset);
+
+        int numberOfMessagesToRead = 5;
+        int numberOfMessagesReadSoFar = 0;
+        boolean keepOnReading = true;
+
+        while (keepOnReading) {
             ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(100));
             for(ConsumerRecord<String,String> record:records){
+                numberOfMessagesReadSoFar += 1;
                 logger.info("Key : "+record.key()+", Value: "+record.value());
                 logger.info("Partition : "+record.partition()+", Offset: "+record.offset());
+                if(numberOfMessagesReadSoFar >= numberOfMessagesToRead){
+                    keepOnReading = false;
+                    break;
+                }
             }
         }
 
+        logger.info("Application is Exiting");
 
     }
 }
